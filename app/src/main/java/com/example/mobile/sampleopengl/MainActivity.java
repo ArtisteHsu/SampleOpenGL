eb882686d366144c5a6d00f011afa38432f7bc31
Artiste Hsu <chhsu@nvidia.com>
2015-05-19 09:01:13 +0800

    Check device supported Open GLES version and set ELG context client version to 2.

--- app/src/main/java/com/example/mobile/sampleopengl/MainActivity.java
+++ app/src/main/java/com/example/mobile/sampleopengl/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.mobile.sampleopengl;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (!supportsEs2) {
+            return;
+        }
         glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setEGLContextClientVersion(2);
         glRenderer = new SampleGLRenderer();
         glSurfaceView.setRenderer(glRenderer);
         setContentView(glSurfaceView);
